3cfd00b8384ccafbf37b1da1ad06b2d9e1c094a3
kelvingcr <contato@kelvindev.tk>
2022-04-05 12:08:07 -0300

    Enviando dados de registro para o firebase

--- app/src/main/java/br/com/kelvingcr/task/ui/auth/RegisterFragment.kt
+++ app/src/main/java/br/com/kelvingcr/task/ui/auth/RegisterFragment.kt
@@ -1,20 +1,79 @@
 package br.com.kelvingcr.task.ui.auth
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
 import br.com.kelvingcr.task.R
+import br.com.kelvingcr.task.databinding.FragmentRegisterBinding
+import br.com.kelvingcr.task.databinding.FragmentSplashBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterFragment : Fragment() {
+
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
+
+        initClicks();
+    }
+
+    private fun validateData() {
+        val email = binding.edtEmail.text.toString().trim();
+        val password = binding.edtPassword.text.toString().trim();
+
+        if (email.isNotEmpty()) {
+            if (password.isNotEmpty()) {
+                binding.progressBar.isVisible = true
+                registerUser(email, password)
+            } else {
+                Toast.makeText(requireContext(), "Informe sua senha", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(requireContext(), "Informe seu email", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initClicks(){
+        binding.btnRegister.setOnClickListener { validateData() }
+    }
+
+    private fun registerUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_registerFragment_to_homeFragment)
+                } else {
+                    binding.progressBar.isVisible = false
+                }
+
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
